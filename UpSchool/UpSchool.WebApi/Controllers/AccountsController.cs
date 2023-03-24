ef024c0b12ac7b7b753cf0f351056d8597eed575
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2023-03-24 16:30:57 +0300

    guid added

--- UpSchool/UpSchool.WebApi/Controllers/AccountsController.cs
+++ UpSchool/UpSchool.WebApi/Controllers/AccountsController.cs
@@ -12,7 +12,7 @@ namespace UpSchool.WebApi.Controllers
         private static List<Account> _accounts = new() {
             new Account()
             {
-                Id = Guid.NewGuid(),
+                Id = new Guid("0bb85132-e3fa-4229-a3cf-f817baa418f5"),
                 UserName = "mrpicle",
                 Password = StringHelper.Base64Encode("123picle123"),
                 IsFavourite = false,
@@ -23,7 +23,7 @@ namespace UpSchool.WebApi.Controllers
         
             new Account()
             {
-            Id = Guid.NewGuid(),
+            Id = new Guid("f4c43715-59d6-4806-9ee9-8cf8a1de8d19"),
             UserName = "fullspeed@gmail.com",
             Password = StringHelper.Base64Encode("123fullspeed123"),
             IsFavourite = true,
@@ -33,7 +33,7 @@ namespace UpSchool.WebApi.Controllers
             },
             new Account()
             { 
-            Id = Guid.NewGuid(),
+            Id = new Guid("bf5f7461-becc-46f8-b4cd-a39b8e6ca626"),
             UserName = "movieguy",
             Password = StringHelper.Base64Encode("123movieguy123"),
             IsFavourite = true,
@@ -49,6 +49,18 @@ namespace UpSchool.WebApi.Controllers
             var accountDtos = _accounts.Select(account => AccountDto.MapFromAccount(account));
             
             return Ok(accountDtos);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            var account = _accounts.FirstOrDefault(x => x.Id == id);
+            if (account is null)
+            {
+                return NotFound("The selected account was not found.");
+            }
+
+            return Ok(AccountDto.MapFromAccount(account));
         }
 
         [HttpPost]
@@ -69,7 +81,14 @@ namespace UpSchool.WebApi.Controllers
             
             return Ok(AccountDto.MapFromAccount(account));
         }
+        
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
+        {
+            var account = _accounts.FirstOrDefault(x => x.Id == id);
+            if (account is null) return NotFound("The selected account was not found");
+            _accounts.Remove(account);
+            return NoContent();
+        }
     }
-    
-    
 }
