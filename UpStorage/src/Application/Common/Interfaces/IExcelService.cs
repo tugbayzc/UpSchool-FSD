5531fcaeea4b7300dba250b5e27a48d0f3686b0b
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2023-04-05 16:56:18 +0300

    entity updates added

--- UpStorage/src/Application/Common/Interfaces/IExcelService.cs
+++ UpStorage/src/Application/Common/Interfaces/IExcelService.cs
@@ -5,4 +5,5 @@ namespace Application.Common.Interfaces;
 public interface IExcelService
 {
     List<ExcelCityDto> ReadCities( ExcelBase64Dto excelDto);
+    List<ExcelCountryDto> ReadCountries(ExcelBase64Dto excelDto);
 }
--- UpStorage/src/Application/Common/Models/Excel/ExcelBase64Dto.cs
+++ UpStorage/src/Application/Common/Models/Excel/ExcelBase64Dto.cs
@@ -3,4 +3,13 @@ namespace Application.Common.Models.Excel;
 public class ExcelBase64Dto
 {
     public string File { get; set; }
+    public ExcelBase64Dto()
+    {
+
+    }
+
+    public ExcelBase64Dto(string file)
+    {
+        File =file;
+    }
 }
