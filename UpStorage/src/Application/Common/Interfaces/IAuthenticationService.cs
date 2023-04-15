d2b46430c24db6529aabe31ec6f8c889fa60a8df
Tuğba Yazıcı <106247691+tugbayzc@users.noreply.github.com>
2023-04-15 18:03:05 +0300

    time out

---
+++ UpStorage/src/Application/Common/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+using Application.Common.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Common.Interfaces;
+
+public interface IAuthenticationService
+{
+    Task<string> CreateUserAsync(CreateUserDto createUserDto, CancellationToken cancellationToken);
+    Task<string> GenerateEmailActivationTokenAsync(string userId, CancellationToken cancellationToken);
+
+}
---
+++ UpStorage/src/Application/Features/Auth/Commands/Register/AuthRegisterCommandHandler.cs
@@ -0,0 +1,27 @@
+using Application.Common.Interfaces;
+using Application.Common.Models.Auth;
+using MediatR;
+
+namespace Application.Features.Auth.Commands.Register;
+
+public class AuthRegisterCommandHandler:IRequestHandler<AuthRegisterCommand, AuthRegisterDto>
+{
+
+    private readonly IAuthenticationService _authenticationService;
+
+    public AuthRegisterCommandHandler(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    public async Task<AuthRegisterDto> Handle(AuthRegisterCommand request, CancellationToken cancellationToken)
+    {
+        var createUserDto = new CreateUserDto(request.FirstName, request.LastName, request.Email, request.Password);
+        var userId = await _authenticationService.CreateUserAsync(createUserDto, cancellationToken);
+        var emailToken = await _authenticationService.GenerateEmailActivationTokenAsync(userId, cancellationToken);
+
+        var fullName = $"{request.FirstName}{request.LastName}";
+
+        return new AuthRegisterDto(request.Email, fullName, emailToken);
+    }
+}
