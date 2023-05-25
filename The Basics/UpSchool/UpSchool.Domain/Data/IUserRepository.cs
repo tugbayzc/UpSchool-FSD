bcbfa6efaf984593900be003f4e6472f3ae02fc1
tugbayazici <yazicittugba@gmail.com>
2023-05-25 12:21:44 +0300

    repository pattern added for the sqlite db connection

---
+++ The Basics/UpSchool/UpSchool.Domain/Data/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using UpSchool.Domain.Entities;
+
+namespace UpSchool.Domain.Data
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllAsync(CancellationToken cancellationToken);  
+
+        Task<User> GetByIdAsync(Guid id,CancellationToken cancellationToken);
+
+        Task<int> AddAsync(User user, CancellationToken cancellationToken);
+
+        Task<int> UpdateAsync(User user, CancellationToken cancellationToken);
+
+        Task<int> DeleteAsync(Guid id,CancellationToken cancellationToken);
+
+    }
+}
---
+++ The Basics/UpSchool/UpSchool.Domain/Data/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using UpSchool.Domain.Entities;
+
+namespace UpSchool.Domain.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public UserRepository(SQLiteAsyncConnection database)
+        {
+            var dbPath = Path.Combine("C:\\Users\\tugbayazici\\Desktop", "upschool.db");
+
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            _database.CreateTableAsync<User>().Wait();
+        }
+
+        public Task<int> AddAsync(User user, CancellationToken cancellationToken)
+        {
+
+            return _database.InsertAsync(user);
+        }
+
+        public Task<int> DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return _database.Table<User>().DeleteAsync(x=> x.Id == id);
+        }
+
+
+        public Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return _database.Table<User>().FirstOrDefaultAsync(x=> x.Id == id);
+        }
+
+        public Task<int> UpdateAsync(User user, CancellationToken cancellationToken)
+        {
+            return _database.UpdateAsync(user);
+        }
+
+        Task<List<User>> IUserRepository.GetAllAsync(CancellationToken cancellationToken)
+        {
+            return _database.Table<User>().ToListAsync();
+        }
+    }
+}
